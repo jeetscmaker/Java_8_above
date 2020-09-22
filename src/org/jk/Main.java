330ac4b7b06fda0c5f5a804739c6a5560254e189
Jitendra <jeetscmaker@gmail.com>
2020-09-22 23:53:00 +0530

    simple comparator using java 8.

---
+++ src/org/jk/Main.java
@@ -0,0 +1,24 @@
+package org.jk;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Sam"));
+        users.add(new User(4, "Joseph"));
+        users.add(new User(6, "Robert"));
+        users.add(new User(7, "Colin"));
+        users.add(new User(11, "Danny"));
+        users.add(new User(5, "Kristine"));
+        System.out.println("Before sorting the list:");
+        System.out.println(users);
+        System.out.println("After sorting the list by Id:");
+        Collections.sort(users, Comparator.comparing(user -> user.getId()));
+        System.out.println(users);
+
+        System.out.println("After sorting the list by name:");
+        Collections.sort(users, Comparator.comparing(user -> user.getName()));
+        System.out.println(users);
+    }
+}
