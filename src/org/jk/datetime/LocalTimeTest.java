2f953bf0ed8bcfe7c49ffa5ddfdbecdab787a42f
Jitendra <jeetscmaker@gmail.com>
2020-12-05 23:08:30 +0530

    first commit of LocalTimeTest.java

---
+++ src/org/jk/datetime/LocalTimeTest.java
@@ -0,0 +1,10 @@
+package org.jk.datetime;
+
+/**
+ * This class shows features of LocalTime class from Java 8 onwards.
+ */
+public class LocalTimeTest {
+    public static void main(String[] args) {
+
+    }
+}
