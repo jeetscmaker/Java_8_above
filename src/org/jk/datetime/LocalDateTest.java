78301098b7ad01850e7022e967b02640def2a1e1
Jitendra <jeetscmaker@gmail.com>
2020-12-06 00:48:42 +0530

    few more methods of LocalDate class.

--- src/org/jk/datetime/LocalDateTest.java
+++ src/org/jk/datetime/LocalDateTest.java
@@ -2,11 +2,13 @@ package org.jk.datetime;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 
 /**
  * Test the features of LocalDate class from Java 8.
- * */
+ */
 public class LocalDateTest {
     public static void main(String[] args) {
         LocalDate localDate = LocalDate.now();
@@ -20,7 +22,26 @@ public class LocalDateTest {
         System.out.println(sunday);
         int twelve = LocalDate.parse("2016-06-12").getDayOfMonth();
         System.out.println(twelve);
+
         //We can test if a date occurs in a leap year. In this example we test if the current date occurs is a leap year:
         System.out.println(LocalDate.now().isLeapYear());
+
+        //The relationship of a date to another can be determined to occur before or after another date:
+        boolean notBefore = LocalDate.parse("2016-06-12")
+                .isBefore(LocalDate.parse("2016-06-11"));
+        System.out.println(notBefore);
+        boolean isAfter = LocalDate.parse("2016-06-12")
+                .isAfter(LocalDate.parse("2016-06-11"));
+        System.out.println(isAfter);
+
+       /* Date boundaries can be obtained from a given date. In the following two examples we get the
+        LocalDateTime that represents the beginning of the day (2016-06-12T00:00) of the given date
+        and the LocalDate that represents the beginning of the month (2016-06-01) respectively:
+        */
+        LocalDateTime beginningOfDay = LocalDate.parse("2016-06-12").atStartOfDay();
+        LocalDate firstDayOfMonth = LocalDate.parse("2016-06-12")
+                .with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(beginningOfDay);
+        System.out.println(firstDayOfMonth);
     }
 }
