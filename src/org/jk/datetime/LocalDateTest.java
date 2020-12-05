57f2ebcf8f419a9b1669b45a18152b06ac64831a
Jitendra <jeetscmaker@gmail.com>
2020-12-05 23:24:13 +0530

    first commit of LocalTimeTest.java

--- src/org/jk/datetime/LocalDateTest.java
+++ src/org/jk/datetime/LocalDateTest.java
@@ -1,9 +1,13 @@
 package org.jk.datetime;
 
+import java.time.LocalDate;
+
 /**
  * Test the features of LocalDate class from Java 8.
  * */
 public class LocalDateTest {
     public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
     }
 }
