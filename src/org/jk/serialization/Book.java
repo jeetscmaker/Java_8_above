95e329852229730eb0b08941de12446bea824d7e
Jitendra <jeetscmaker@gmail.com>
2020-12-07 23:56:45 +0530

    getters, setters and constructors created for Book class.

--- src/org/jk/serialization/Book.java
+++ src/org/jk/serialization/Book.java
@@ -3,7 +3,63 @@ package org.jk.serialization;
 import java.io.Serializable;
 
 /**
- * We will various aspects of serialization with the use of  this class
+ * We will test various aspects of serialization with the use of  this class
  */
 public class Book implements Serializable {
+    private String bookName;
+    private String isbn;
+    private String author;
+    private String publisher;
+    private double price;
+
+    public Book() {
+    }
+
+    public Book(String b, String i, String a, String p, double pr) {
+        bookName = b;
+        isbn = i;
+        author = a;
+        publisher = p;
+        price = pr;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
