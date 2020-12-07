8277ececa83fb999115f2db37229d1663ef21c50
Jitendra <jeetscmaker@gmail.com>
2020-12-07 23:34:13 +0530

    Serialization: first commit, Book implements Serializable.

---
+++ src/org/jk/serialization/Book.java
@@ -0,0 +1,9 @@
+package org.jk.serialization;
+
+import java.io.Serializable;
+
+/**
+ * We will various aspects of serialization with the use of  this class
+ */
+public class Book implements Serializable {
+}
