8a780513e5e7cea5bf99d3258df1baf45560f138
Jitendra <jeetscmaker@gmail.com>
2020-12-07 23:36:14 +0530

    Serialization: A test serialization class.

---
+++ src/org/jk/serialization/SerializationTest.java
@@ -0,0 +1,10 @@
+package org.jk.serialization;
+
+/**
+ * A test class for testing java serialization properties
+ */
+public class SerializationTest {
+    public static void main(String[] args) {
+
+    }
+}
