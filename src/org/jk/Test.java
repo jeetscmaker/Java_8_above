888d24e85fd520c2926073225ef03119f9c1a4cd
Jitendra <jeetscmaker@gmail.com>
2021-02-14 23:54:20 +0530

    random string of length 10 with only lowercase alphabets

--- src/org/jk/Test.java
+++ src/org/jk/Test.java
@@ -10,5 +10,20 @@ public class Test {
         new Random().nextBytes(array);
         String generatedString = new String(array, Charset.forName("UTF-8"));
         System.out.println(generatedString);
+
+        // random string of length 10 with only lowercase alphabets
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        generatedString = buffer.toString();
+
+        System.out.println(generatedString);
     }
 }
