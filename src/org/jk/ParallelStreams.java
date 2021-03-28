83f0df04420f9be0c71a3f85956ab2acfe59e95e
Jitendra <jeetscmaker@gmail.com>
2021-03-28 21:29:32 +0530

    comments added for better understanding.

--- src/org/jk/ParallelStreams.java
+++ src/org/jk/ParallelStreams.java
@@ -4,6 +4,8 @@ import java.util.stream.IntStream;
 
 public class ParallelStreams {
     public static void main(String[] args) {
+        // Normal stream performs entire operation on single core.
+        // below operation takes 3 ms.
         IntStream intStream = IntStream.range(1, 100000);
         long start = System.currentTimeMillis();
         long k = intStream.map(i -> i * 2).count();
@@ -12,6 +14,8 @@ public class ParallelStreams {
 
         System.out.println("==========================================");
 
+        // Parallel stream performs entire operation on
+        // below operation takes 1 ms.
         IntStream parallelStream = IntStream.range(1, 100000).parallel();
         start = System.currentTimeMillis();
         k = parallelStream.map(i -> i * 2).count();
