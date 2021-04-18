2c2adc2ed658a27f31587c276f839ebb5ae61eb8
Jitendra <jeetscmaker@gmail.com>
2021-04-18 11:20:12 +0530

    calculate factorial using BigInteger in Java.

--- src/org/jk/A.java
+++ src/org/jk/A.java
@@ -1,56 +1,25 @@
 package org.jk;
 
-/* Java program to demonstrate
- the behavior of the
- submit() method */
-
-import java.util.concurrent.*;
+import java.math.BigInteger;
 
 public class A {
-    public static void main(String[] args)
-            throws Exception {
-        Thread t = new Thread(new Job());
-        t.start();
-        // Creating the object of the
-        // Executor service interface
-        ExecutorService executorService
-                = Executors.newFixedThreadPool(1);
-
-        // submit() method can return the
-        // result of the computation
-        // because it has a return type of Future.
-
-        // By using submit(), we are
-        // accepting a Callable task
-        Future obj = executorService.submit(new Callable() {
-
-            // Overriding the call method
-            public Object call() {
-                System.out.println(
-                        "This is submit() "
-                                + "method example");
-
-                return "Returning Callable "
-                        + "Task Result";
-            }
-        });
-
-        /* This method will return the result
-         if the task has finished perfectly.
-         The submit() method returns a
-         Java Future object which is
-         used to check when the Runnable
-         has completed.
-         As it implements Future,
-         get() method is called
-         to get the result
-         */
-        System.out.println(obj.get());
+    public static void main(String[] args) {
+        int n = 50;
+        BigInteger factorial = findFactorial(n);
+        System.out.println(factorial);
     }
-}
-class Job implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("Job done!");
+
+    private static BigInteger findFactorial(int n) {
+        if (n > 50 || n < 0) {
+            throw new RuntimeException("Input should not be greater than 20 and negative.");
+        }
+        if (n <= 1) {
+            return BigInteger.ONE;
+        }
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(new BigInteger(String.valueOf(i)));
+        }
+        return result;
     }
 }
